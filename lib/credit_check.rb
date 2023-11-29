b4ee752023b5ebc3bb9c3c1ad0500bcadfcad421
Odell McFarland IV <odell.mac4@gmail.com>
2023-11-28 21:41:07 -0800

    Validate the credit card number

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,4 +1,34 @@
-card_number = "5541808923795240"
+require './lib/credit_card.rb'
+
+card_number = "4024007106512380"
+
+def luhn_algo(card_number)
+card_number_array = card_number.chars
+digits = card_number_array.map  {|char| char.to_i }
+double_digits = digits.map.with_index do |digit, index|
+            if index.even?
+                digit * 2
+            else
+                digit 
+        end
+    end
+    summed_digits = double_digits.map do |digit|
+        if digit > 9
+            digit.digits.sum
+        else
+            digit
+        end
+    end
+# p `summed_digits.sum` Check the sum of new array to see if divisible by 10
+    if summed_digits.sum % 10 == 0
+        "The number #{card_number} is valid!"
+    else
+        "The number #{card_number} is invalid!"
+    end
+end
+
+luhn_algo(card_number)
+
 
 # Your Luhn Algorithm Here
 
